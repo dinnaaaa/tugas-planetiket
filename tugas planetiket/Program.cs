7f44d03dde1a9cb09ea7b162cdb607fb6b4d7154
dinnaaaa <Dinakartikapramudita@students.amikom.ac.id>
2023-04-05 15:44:42 +0700

    Add project files.

---
+++ tugas planetiket/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tugas_planetiket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PlaneTicket ticket1 = new PlaneTicket();
+            PlaneTicket ticket2 = new PlaneTicket();
+
+            ticket1.Origin = "Jogja";
+            ticket1.Destination = "YIA";
+            ticket1.Cost = 800000;
+            ticket1.Currency = "IDR";
+
+            ticket2.Origin = "Korea";
+            ticket2.Destination = "PKU";
+            ticket2.Cost = 200;
+            ticket2.Currency = "USD";
+
+            ticket1.PrintSummary();
+            Console.WriteLine();
+            ticket2.PrintSummary();
+            Console.ReadKey();
+        }
+    }
+}
